625ff2c273930e3f6802c46ad2e06528dd050b42
Nguyen-Tuan-Kiet27 <0306221133@caothang.edu.vn>
2025-06-18 14:08:31 +0700

    hoàn thành chức năng tạo tóm tắt bằng AI

--- NovelNest/routes/api.php
+++ NovelNest/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User_Controller;
 use App\Http\Controllers\Admin_Controller;
 use App\Http\Controllers\Author_Controller;
+use App\Http\Controllers\Summary_Controller;
 
 
 Route::get('/user', function (Request $request) {
@@ -17,6 +18,7 @@ Route::post('/logout',[User_Controller::class,'logout']);
 //Author
 Route::post('/author/themtruyen', [Author_Controller::class,'apiThemTruyen'])->middleware(['web','CheckLogin:1,2,3']);
 Route::post('/author/themchuong/{id}', [Author_Controller::class,'apiThemChuong'])->middleware(['web','CheckLogin:1,2,3']);
+Route::post('/tomtat', [Summary_Controller::class,'summarize'])->middleware(['web','CheckLogin:1,2,3']);
 
 //Admin
 Route::post('/admin/login', [Admin_Controller::class,'authLogin'])->middleware(['web']);
